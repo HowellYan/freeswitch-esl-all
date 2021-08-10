46d89ea81f578da32c4902ea86f79f45a0cb8cb3
Howell <th15817161961@gmail.com>
2021-08-11 00:06:42 +0800

    init

---
+++ opensip-event/src/main/java/link/thingscloud/opensips/event/constant/VariableConstant.java
@@ -0,0 +1,13 @@
+package link.thingscloud.opensips.event.constant;
+
+/**
+ * @author zhouhailin
+ * @version 1.5.0
+ */
+public class VariableConstant {
+
+    public static final String PREFIX = "variable_";
+
+    private VariableConstant() {
+    }
+}
